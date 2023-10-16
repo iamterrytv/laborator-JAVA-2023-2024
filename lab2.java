8a6006efcbdcdde2c168b98bc7d35b899438db44
iamterrytv <ticarat.terriusvalentin@student.uoradea.ro>
2023-10-16 20:19:15 +0300

    lab2

--- lab2.java
+++ lab2.java
@@ -1,6 +1,9 @@
-public class lab2
-{
-    public static void main (String [] args) {
-        System.out.println("First Java Application");
+public class lab2 {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 1; i <= n; i++) {
+            int p = i * i;
+            System.out.println(p);
+        }
     }
 }
