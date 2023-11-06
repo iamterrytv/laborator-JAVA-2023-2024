2fa1ab2493396956f7992aac4c5285c7d3b6c545
iamterrytv <ticarat.terriusvalentin@student.uoradea.ro>
2023-11-06 19:35:58 +0200

    .

---
+++ src/lab5/Cat.java
@@ -0,0 +1,20 @@
+package lab5;
+
+public class Cat {
+    private String name;
+    private String colour;
+    private Integer age;
+    private Integer energyLevel;
+    private Integer hungerLevel;
+    private Integer moodLevel;
+
+    public Cat(String name, String colour, Integer age)
+    {
+        this.name=name;
+        this.colour=colour;
+        this.age=age;
+        this.energyLevel=3;
+        this.hungerLevel=3;
+        this.moodLevel=3;
+    }
+}
