99ed19fa63c76de5e04fab5731af7739f9a0e171
iamterrytv <ticarat.terriusvalentin@student.uoradea.ro>
2023-11-06 20:27:06 +0200

    .

--- src/lab5/Main.java
+++ src/lab5/Main.java
@@ -1,15 +1,36 @@
 package lab5;
 import java.util.Scanner;
-public class Main {
-    public static void Main(String[] args)
-    {
-        Cat firstCat = new Cat("Tom", "white",16);
-        System.out.println("Cat name"+firstCat.getName());
-        System.out.println("Cat colour"+firstCat.getColour());
-        System.out.println("Cat age"+ firstCat.getAge());
 
-        firstCat.play();
-        firstCat.feed();
-        firstCat.sleep();
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter Employee details
+        System.out.print("Enter Employee Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter Employee Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Enter Hourly Rate: ");
+        double hourRate = scanner.nextDouble();
+
+        System.out.print("Enter Working Capacity (hours per day): ");
+        int capacity = scanner.nextInt();
+
+        System.out.print("Enter Free Days: ");
+        int freeDays = scanner.nextInt();
+
+
+        Employee employee = new Employee(name, email, hourRate, capacity, freeDays);
+
+
+        int dailyIncome = employee.calculateDailyIncome();
+        System.out.println("Daily Income: $" + dailyIncome);
+
+
+        employee.printMonthlyIncome();
+
+        scanner.close();
     }
 }
