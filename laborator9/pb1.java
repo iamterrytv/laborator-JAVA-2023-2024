3fe05063401d9ab6793d37e5e7c784ff46d0d45f
iamterrytv <ticarat.terriusvalentin@student.uoradea.ro>
2023-11-27 18:33:17 +0200

    lab9

---
+++ laborator9/pb1.java
@@ -0,0 +1,17 @@
+package laborator9;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class pb1{
+    public static void main(String[] args) {
+        List<String> listaCuDuplicate = Arrays.asList("a", "b", "c", "b", "d", "a", "e");
+
+        List<String> listaFaraDuplicate = listaCuDuplicate.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        System.out.println("Lista cu duplicate: " + listaCuDuplicate);
+        System.out.println("Lista fără duplicate: " + listaFaraDuplicate);
+    }
+}
